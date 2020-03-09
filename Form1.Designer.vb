3b79c3f4dc8d420b47820ec2c42213c0b4ef3edb
zyllemunar <oliverzylle@gmail.com>
2020-03-09 12:50:46 +0800

    initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.imageBox1 = New Emgu.CV.UI.ImageBox()
+        Me.label3 = New System.Windows.Forms.Label()
+        Me.imageBoxFrameGrabber = New Emgu.CV.UI.ImageBox()
+        Me.button2 = New System.Windows.Forms.Button()
+        Me.label5 = New System.Windows.Forms.Label()
+        Me.label4 = New System.Windows.Forms.Label()
+        Me.groupBox1 = New System.Windows.Forms.GroupBox()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.textBox1 = New System.Windows.Forms.TextBox()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label6 = New System.Windows.Forms.Label()
+        CType(Me.imageBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.imageBoxFrameGrabber, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.groupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'imageBox1
+        '
+        Me.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.imageBox1.Location = New System.Drawing.Point(11, 18)
+        Me.imageBox1.Name = "imageBox1"
+        Me.imageBox1.Size = New System.Drawing.Size(106, 89)
+        Me.imageBox1.TabIndex = 5
+        Me.imageBox1.TabStop = False
+        '
+        'label3
+        '
+        Me.label3.AutoSize = True
+        Me.label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label3.ForeColor = System.Drawing.Color.Red
+        Me.label3.Location = New System.Drawing.Point(198, 311)
+        Me.label3.Name = "label3"
+        Me.label3.Size = New System.Drawing.Size(16, 16)
+        Me.label3.TabIndex = 15
+        Me.label3.Text = "0"
+        '
+        'imageBoxFrameGrabber
+        '
+        Me.imageBoxFrameGrabber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.imageBoxFrameGrabber.Location = New System.Drawing.Point(12, 12)
+        Me.imageBoxFrameGrabber.Name = "imageBoxFrameGrabber"
+        Me.imageBoxFrameGrabber.Size = New System.Drawing.Size(320, 240)
+        Me.imageBoxFrameGrabber.TabIndex = 13
+        Me.imageBoxFrameGrabber.TabStop = False
+        '
+        'button2
+        '
+        Me.button2.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.button2.Location = New System.Drawing.Point(11, 158)
+        Me.button2.Name = "button2"
+        Me.button2.Size = New System.Drawing.Size(106, 31)
+        Me.button2.TabIndex = 3
+        Me.button2.Text = "Train"
+        Me.button2.UseVisualStyleBackColor = True
+        '
+        'label5
+        '
+        Me.label5.AutoSize = True
+        Me.label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label5.ForeColor = System.Drawing.Color.Black
+        Me.label5.Location = New System.Drawing.Point(12, 255)
+        Me.label5.Name = "label5"
+        Me.label5.Size = New System.Drawing.Size(197, 15)
+        Me.label5.TabIndex = 17
+        Me.label5.Text = "Persons present in the scene:"
+        '
+        'label4
+        '
+        Me.label4.AutoSize = True
+        Me.label4.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label4.ForeColor = System.Drawing.Color.Blue
+        Me.label4.Location = New System.Drawing.Point(12, 281)
+        Me.label4.Name = "label4"
+        Me.label4.Size = New System.Drawing.Size(45, 19)
+        Me.label4.TabIndex = 16
+        Me.label4.Text = "........."
+        '
+        'groupBox1
+        '
+        Me.groupBox1.Controls.Add(Me.label1)
+        Me.groupBox1.Controls.Add(Me.textBox1)
+        Me.groupBox1.Controls.Add(Me.imageBox1)
+        Me.groupBox1.Controls.Add(Me.button2)
+        Me.groupBox1.Location = New System.Drawing.Point(338, 42)
+        Me.groupBox1.Name = "groupBox1"
+        Me.groupBox1.Size = New System.Drawing.Size(126, 210)
+        Me.groupBox1.TabIndex = 14
+        Me.groupBox1.TabStop = False
+        Me.groupBox1.Text = "Training: "
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(8, 119)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(41, 13)
+        Me.label1.TabIndex = 8
+        Me.label1.Text = "Name: "
+        '
+        'textBox1
+        '
+        Me.textBox1.Location = New System.Drawing.Point(11, 135)
+        Me.textBox1.Name = "textBox1"
+        Me.textBox1.Size = New System.Drawing.Size(103, 20)
+        Me.textBox1.TabIndex = 7
+        Me.textBox1.Text = "----"
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label2.Location = New System.Drawing.Point(13, 311)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(179, 15)
+        Me.label2.TabIndex = 14
+        Me.label2.Text = "Number of faces detected: "
+        '
+        'button1
+        '
+        Me.button1.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.button1.Location = New System.Drawing.Point(12, 330)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(452, 36)
+        Me.button1.TabIndex = 2
+        Me.button1.Text = "Start"
+        Me.button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(338, 9)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(130, 16)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "Face Recognition"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(473, 372)
+        Me.Controls.Add(Me.button1)
+        Me.Controls.Add(Me.label3)
+        Me.Controls.Add(Me.label4)
+        Me.Controls.Add(Me.label2)
+        Me.Controls.Add(Me.label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.imageBoxFrameGrabber)
+        Me.Controls.Add(Me.groupBox1)
+        Me.name = "Form1"
+        Me.Text = "Face Recognition"
+        CType(Me.imageBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.imageBoxFrameGrabber, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.groupBox1.ResumeLayout(False)
+        Me.groupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents imageBox1 As Emgu.CV.UI.ImageBox
+    Private WithEvents label3 As System.Windows.Forms.Label
+    Private WithEvents imageBoxFrameGrabber As Emgu.CV.UI.ImageBox
+    Private WithEvents button2 As System.Windows.Forms.Button
+    Private WithEvents label5 As System.Windows.Forms.Label
+    Private WithEvents label4 As System.Windows.Forms.Label
+    Private WithEvents groupBox1 As System.Windows.Forms.GroupBox
+    Private WithEvents label1 As System.Windows.Forms.Label
+    Private WithEvents textBox1 As System.Windows.Forms.TextBox
+    Private WithEvents label2 As System.Windows.Forms.Label
+    Private WithEvents button1 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,319 @@
+﻿Imports System.Diagnostics
+Imports Emgu.CV.Structure
+Imports Emgu.CV
+Imports Emgu.CV.CvEnum
+Imports System.Collections.Generic
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports System.IO
+
+Public Class Form1
+    Dim currentFrame As Image(Of Bgr, [Byte])
+    Dim grabber As Capture
+    Dim face As HaarCascade
+    Dim eye As HaarCascade
+    Dim font As New MCvFont(CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX, 0.5, 0.5)
+    Dim result As Image(Of Gray, Byte), TrainedFace As Image(Of Gray, Byte) = Nothing
+    Dim gray As Image(Of Gray, Byte) = Nothing
+    Dim trainingImages As New List(Of Image(Of Gray, Byte))()
+    Dim labels As New List(Of String)()
+    Dim NamePersons As New List(Of String)()
+    Dim ContTrain As Integer, NumLabels As Integer, t As Integer
+    Dim name As String, names As String = Nothing
+
+    <Serializable()> _
+    Public Class EigenObjectRecognizer
+        Private _eigenImages As Image(Of Gray, [Single])()
+        Private _avgImage As Image(Of Gray, [Single])
+        Private _eigenValues As Matrix(Of Single)()
+        Private _labels As String()
+        Private _eigenDistanceThreshold As Double
+
+  
+        Public Property EigenImages() As Image(Of Gray, [Single])()
+            Get
+                Return _eigenImages
+            End Get
+            Set(ByVal value As Image(Of Gray, [Single])())
+                _eigenImages = value
+            End Set
+        End Property
+
+       
+        Public Property Labels() As [String]()
+            Get
+                Return _labels
+            End Get
+            Set(ByVal value As [String]())
+                _labels = value
+            End Set
+        End Property
+
+        
+        Public Property EigenDistanceThreshold() As Double
+            Get
+                Return _eigenDistanceThreshold
+            End Get
+            Set(ByVal value As Double)
+                _eigenDistanceThreshold = value
+            End Set
+        End Property
+
+       
+        Public Property AverageImage() As Image(Of Gray, [Single])
+            Get
+                Return _avgImage
+            End Get
+            Set(ByVal value As Image(Of Gray, [Single]))
+                _avgImage = value
+            End Set
+        End Property
+
+       
+        Public Property EigenValues() As Matrix(Of Single)()
+            Get
+                Return _eigenValues
+            End Get
+            Set(ByVal value As Matrix(Of Single)())
+                _eigenValues = value
+            End Set
+        End Property
+
+        Private Sub New()
+        End Sub
+
+
+
+        Public Sub New(ByVal images As Image(Of Gray, [Byte])(), ByRef termCrit As MCvTermCriteria)
+            Me.New(images, GenerateLabels(images.Length), termCrit)
+        End Sub
+
+        Private Shared Function GenerateLabels(ByVal size As Integer) As [String]()
+            Dim labels As [String]() = New String(size - 1) {}
+            For i As Integer = 0 To size - 1
+                labels(i) = i.ToString()
+            Next
+            Return labels
+        End Function
+
+       
+        Public Sub New(ByVal images As Image(Of Gray, [Byte])(), ByVal labels As [String](), ByRef termCrit As MCvTermCriteria)
+            Me.New(images, labels, 0, termCrit)
+        End Sub
+
+        
+        Public Sub New(ByVal images As Image(Of Gray, [Byte])(), ByVal labels As [String](), ByVal eigenDistanceThreshold As Double, ByRef termCrit As MCvTermCriteria)
+            Debug.Assert(images.Length = labels.Length, "The number of images should equals the number of labels")
+            Debug.Assert(eigenDistanceThreshold >= 0.0, "Eigen-distance threshold should always >= 0.0")
+
+            CalcEigenObjects(images, termCrit, _eigenImages, _avgImage)
+
+           
+
+            _eigenValues = Array.ConvertAll(Of Image(Of Gray, [Byte]), Matrix(Of Single))(images, Function(img As Image(Of Gray, [Byte])) New Matrix(Of Single)(EigenDecomposite(img, _eigenImages, _avgImage)))
+
+            _labels = labels
+
+            _eigenDistanceThreshold = eigenDistanceThreshold
+        End Sub
+
+#Region "static methods"
+        ''' <summary>
+        ''' Caculate the eigen images for the specific traning image
+        ''' </summary>
+        ''' <param name="trainingImages">The images used for training </param>
+        ''' <param name="termCrit">The criteria for tranning</param>
+        ''' <param name="eigenImages">The resulting eigen images</param>
+        ''' <param name="avg">The resulting average image</param>
+        Public Shared Sub CalcEigenObjects(ByVal trainingImages As Image(Of Gray, [Byte])(), ByRef termCrit As MCvTermCriteria, ByRef eigenImages As Image(Of Gray, [Single])(), ByRef avg As Image(Of Gray, [Single]))
+            Dim width As Integer = trainingImages(0).Width
+            Dim height As Integer = trainingImages(0).Height
+
+            Dim inObjs As IntPtr() = Array.ConvertAll(Of Image(Of Gray, [Byte]), IntPtr)(trainingImages, Function(img As Image(Of Gray, [Byte])) img.Ptr)
+
+            If termCrit.max_iter <= 0 OrElse termCrit.max_iter > trainingImages.Length Then
+                termCrit.max_iter = trainingImages.Length
+            End If
+
+            Dim maxEigenObjs As Integer = termCrit.max_iter
+
+            '#Region "initialize eigen images"
+            eigenImages = New Image(Of Gray, Single)(maxEigenObjs - 1) {}
+            For i As Integer = 0 To eigenImages.Length - 1
+                eigenImages(i) = New Image(Of Gray, Single)(width, height)
+            Next
+            Dim eigObjs As IntPtr() = Array.ConvertAll(Of Image(Of Gray, [Single]), IntPtr)(eigenImages, Function(img As Image(Of Gray, [Single])) img.Ptr)
+            '#End Region
+
+            avg = New Image(Of Gray, [Single])(width, height)
+
+            CvInvoke.cvCalcEigenObjects(inObjs, termCrit, eigObjs, Nothing, avg.Ptr)
+        End Sub
+
+        ''' <summary>
+        ''' Decompose the image as eigen values, using the specific eigen vectors
+        ''' </summary>
+        ''' <param name="src">The image to be decomposed</param>
+        ''' <param name="eigenImages">The eigen images</param>
+        ''' <param name="avg">The average images</param>
+        ''' <returns>Eigen values of the decomposed image</returns>
+        Public Shared Function EigenDecomposite(ByVal src As Image(Of Gray, [Byte]), ByVal eigenImages As Image(Of Gray, [Single])(), ByVal avg As Image(Of Gray, [Single])) As Single()
+            Return CvInvoke.cvEigenDecomposite(src.Ptr, Array.ConvertAll(Of Image(Of Gray, [Single]), IntPtr)(eigenImages, Function(img As Image(Of Gray, [Single])) img.Ptr), avg.Ptr)
+        End Function
+#End Region
+
+      
+        Public Function EigenProjection(ByVal eigenValue As Single()) As Image(Of Gray, [Byte])
+            Dim res As Image(Of Gray, [Byte]) = New Image(Of Gray, Byte)(_avgImage.Width, _avgImage.Height)
+            CvInvoke.cvEigenProjection(Array.ConvertAll(Of Image(Of Gray, [Single]), IntPtr)(_eigenImages, Function(img As Image(Of Gray, [Single])) img.Ptr), eigenValue, _avgImage.Ptr, res.Ptr)
+            Return res
+        End Function
+
+       
+        Public Function GetEigenDistances(ByVal image As Image(Of Gray, [Byte])) As Single()
+            Using eigenValue As New Matrix(Of Single)(EigenDecomposite(image, _eigenImages, _avgImage))
+                Return Array.ConvertAll(Of Matrix(Of Single), Single)(_eigenValues, Function(eigenValueI As Matrix(Of Single)) CSng(CvInvoke.cvNorm(eigenValue.Ptr, eigenValueI.Ptr, Emgu.CV.CvEnum.NORM_TYPE.CV_L2, IntPtr.Zero)))
+            End Using
+        End Function
+
+        
+        Public Sub FindMostSimilarObject(ByVal image As Image(Of Gray, [Byte]), ByRef index As Integer, ByRef eigenDistance As Single, ByRef label As [String])
+            Dim dist As Single() = GetEigenDistances(image)
+
+            index = 0
+            eigenDistance = dist(0)
+            For i As Integer = 1 To dist.Length - 1
+                If dist(i) < eigenDistance Then
+                    index = i
+                    eigenDistance = dist(i)
+                End If
+            Next
+            label = Labels(index)
+        End Sub
+
+      
+        Public Function Recognize(ByVal image As Image(Of Gray, [Byte])) As [String]
+            Dim index As Integer
+            Dim eigenDistance As Single
+            Dim label As [String]
+            FindMostSimilarObject(image, index, eigenDistance, label)
+
+            Return If((_eigenDistanceThreshold <= 0 OrElse eigenDistance < _eigenDistanceThreshold), _labels(index), [String].Empty)
+        End Function
+    End Class
+
+    Public Sub New()
+        InitializeComponent()
+        face = New HaarCascade("haarcascade_frontalface_default.xml")
+        Try
+            
+            Dim Labelsinfo As String = File.ReadAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt")
+            Dim Labels__1 As String() = Labelsinfo.Split("%"c)
+            NumLabels = Convert.ToInt16(Labels__1(0))
+            ContTrain = NumLabels
+            Dim LoadFaces As String
+
+            For tf As Integer = 1 To NumLabels
+                LoadFaces = "face" & tf & ".bmp"
+                trainingImages.Add(New Image(Of Gray, Byte)(Application.StartupPath + "/TrainedFaces/" & LoadFaces))
+                labels.Add(Labels__1(tf))
+
+            Next
+        Catch e As Exception
+            
+            MessageBox.Show("Empty Database", "Load Faces", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        End Try
+    End Sub
+
+    Private Sub button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles button1.Click
+        grabber = New Capture()
+        grabber.QueryFrame()
+        Timer1.Start()
+        button1.Enabled = False
+    End Sub
+
+    Private Sub button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles button2.Click
+        Try
+            
+            ContTrain = ContTrain + 1
+            gray = grabber.QueryGrayFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC)
+            Dim facesDetected As MCvAvgComp()() = gray.DetectHaarCascade(face, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, New Size(20, 20))
+            For Each f As MCvAvgComp In facesDetected(0)
+                TrainedFace = currentFrame.Copy(f.rect).Convert(Of Gray, Byte)()
+                Exit For
+            Next
+
+            TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC)
+            trainingImages.Add(TrainedFace)
+            labels.Add(textBox1.Text)
+
+            imageBox1.Image = TrainedFace
+
+            File.WriteAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt", trainingImages.ToArray().Length.ToString() & "%")
+
+           
+            For i As Integer = 1 To trainingImages.ToArray().Length
+                trainingImages.ToArray()(i - 1).Save(Application.StartupPath + "/TrainedFaces/face" & i & ".bmp")
+                File.AppendAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt", labels.ToArray()(i - 1) + "%")
+            Next
+
+            MessageBox.Show(textBox1.Text + "'s Face Registered", "Registration Complete", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            textBox1.Text = ""
+            imageBox1.Image = Nothing
+        Catch
+            MessageBox.Show("Enable the face detection first", "Registration Failed", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End Try
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        label3.Text = "0"
+
+        NamePersons.Add("")
+
+
+       
+        currentFrame = grabber.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC)
+
+        gray = currentFrame.Convert(Of Gray, [Byte])()
+
+        Dim facesDetected As MCvAvgComp()() = gray.DetectHaarCascade(face, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, New Size(20, 20))
+
+        For Each f As MCvAvgComp In facesDetected(0)
+            t = t + 1
+            result = currentFrame.Copy(f.rect).Convert(Of Gray, Byte)().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC)
+            
+            currentFrame.Draw(f.rect, New Bgr(Color.Red), 2)
+
+
+            If trainingImages.ToArray().Length <> 0 Then
+               
+                Dim termCrit As New MCvTermCriteria(ContTrain, 0.001)
+
+                Dim recognizer As New EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 3000, termCrit)
+
+                name = recognizer.Recognize(result)
+
+
+                currentFrame.Draw(name, font, New Point(f.rect.X - 2, f.rect.Y - 2), color:=New Bgr(Color.LightGreen))
+            End If
+
+            NamePersons(t - 1) = name
+            NamePersons.Add("")
+
+            label3.Text = facesDetected(0).Length.ToString()
+        Next
+        t = 0
+
+        For nnn As Integer = 0 To facesDetected(0).Length - 1
+            names = names + NamePersons(nnn) + ", "
+        Next
+       
+        imageBoxFrameGrabber.Image = currentFrame
+        label4.Text = names
+        names = ""
+        NamePersons.Clear()
+    End Sub
+
+End Class
